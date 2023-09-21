255e3065075b449718cd4c269a0d7589c8bb3bbc
kumar964050 <kumar964050@gmail.com>
2023-09-21 14:25:57 +0530

    first commit

---
+++ JDBC-Projects/goodreads/src/main/java/com/example/goodreads/model/BookRowMapper.java
@@ -0,0 +1,17 @@
+package com.example.goodreads.model;
+
+import org.springframework.jdbc.core.RowMapper;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import com.example.goodreads.model.Book;
+
+public class BookRowMapper implements RowMapper<Book>{
+    @Override
+    public Book mapRow(ResultSet rs, int rowNum) throws SQLException{
+        return new Book(
+                rs.getInt("id"),
+                rs.getString("name"),
+                rs.getString("imageUrl")
+        );
+    }
+}
